961a5ce0ef5485151dd501e24576a3bd41f9c46a
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-23 01:47:15 +0200

    scusate l'orario oggi spalavo del fango a cesena

--- vite-comics/src/components/AppHero.vue
+++ vite-comics/src/components/AppHero.vue
@@ -5,7 +5,16 @@ export default {
 </script>
 
 <template>
-    <div class="jumbotron bg-secondary">
-        hero
+    <div class="jumbotron">
     </div>
 </template>
+
+<style scoped lang="scss">
+div {
+    height: 400px;
+    background-image: url(../assets/jumbotron.jpg);
+    background-position-y: 0px;
+    background-position-x: 50%;
+    background-repeat: no-repeat;
+}
+</style>
--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -1,19 +1,59 @@
 <script>
 export default {
     name: "AppMain",
+    props: ["items"]
 }
 </script>
 
 <template>
-    <div class="">
-        <h3 class="pt-5">qui va il contenuto</h3>
+    <div class="appMain">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-11 d-flex flex-wrap justify-content-between mb-3">
+                    <template v-for="item in items" class="">
+                        <div class="item_card my-5">
+                            <img class="" :src="item.thumb" alt="">
+                            <p class="mt-3 text-white">{{ item.series }}</p>
+                        </div>
+                    </template>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary px-5 mb-3">load more</button>
     </div>
 </template>
 
-<style scoped>
-div {
-    height: 150px;
-    background-color: rgba(0, 0, 0, 0.8);
-    color: white
+<style scoped lang="scss">
+.appMain {
+    background-color: rgba(0, 0, 0, 0.85);
+    text-align: center;
+}
+
+.row {
+    justify-content: center;
+}
+
+.item_card {
+    width: 180px;
+    height: 180px;
+
+    & :hover {
+        transform: scale(1.05);
+    }
+
+    img {
+        width: 180px;
+        height: 180px;
+        object-fit: cover
+    }
+
+    p {
+        text-align: center;
+        line-break: auto;
+        font-size: small
+    }
+
+    ;
 }
 </style>
